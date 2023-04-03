2d9d67a64ca5f01300bd968cab9bf81a9d734331
Mason Stooksbury <masonstooksbury@gmail.com>
2023-04-02 19:43:14 -0500

    Added a lot of stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import cpu_data from '../assets/cpu-consumption.json';
+import gpu_data from '../assets/gpu-consumption.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the-cost-of-us';
+  selected_cpu = 'CPU';
+  selected_gpu = 'GPU';
+  cpu_list = cpu_data;
+  gpu_list = gpu_data;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
