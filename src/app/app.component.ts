ef6c14c180a89196f67ebbb3d83e590be2cef778
Johnathan Wood <epicshadowkrazee@gmail.com>
2023-04-03 10:49:23 -0400

    Implemented autocompletes and initial form structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
 import cpu_data from '../assets/cpu-consumption.json';
 import gpu_data from '../assets/gpu-consumption.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'the-cost-of-us';
-  selected_cpu = 'CPU';
-  selected_gpu = 'GPU';
-  cpu_list = cpu_data;
-  gpu_list = gpu_data;
 
+  fg = new FormGroup({
+    'cpu': new FormControl('', Validators.required),
+    'gpu': new FormControl('', Validators.required),
+    'kwh': new FormControl('', Validators.required)
+  });
 
-  isCostSubmitDisabled = true;
+  cpu_list = cpu_data.map((d) => d['name']);
+  gpu_list = gpu_data.map((d) => d['name']);
 
-  
+  filtered_cpu_list: string[] = Object.assign([], this.cpu_list);
+  filtered_gpu_list: string[] = Object.assign([], this.gpu_list);
 
-  // myControl = new FormControl('');
-  // options: string[] = ['One', 'Two', 'Three'];
-  // filteredOptions: Observable<string[]> | undefined;
+  cpuControl = new FormControl('');
+  gpuControl = new FormControl('');
 
-  // ngOnInit() {
-  //   this.filteredOptions = this.myControl.valueChanges.pipe(
-  //     startWith(''),
-  //     map(value => this._filter(value || '')),
-  //   );
-  // }
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> | undefined;
 
-  // private _filter(value: string): string[] {
-  //   const filterValue = value.toLowerCase();
+  ngOnInit() {
+    this.fg.controls['cpu'].valueChanges.subscribe((ft) => {
+      console.log('filter!');
+      this.filtered_cpu_list = !!ft ? this.cpu_list.filter((c) => c.toLowerCase().includes(ft.toLowerCase())) : Object.assign([], this.cpu_list);
+    });
 
-  //   return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  // }
+    this.fg.controls['gpu'].valueChanges.subscribe((ft) => {
+      this.filtered_gpu_list = !!ft ? this.gpu_list.filter((c) => c.toLowerCase().includes(ft.toLowerCase())) : Object.assign([], this.gpu_list);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatButtonModule,
     MatSelectModule,
     MatAutocompleteModule,
+    MatInputModule,
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule
